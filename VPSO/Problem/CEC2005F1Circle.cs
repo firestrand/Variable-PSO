cfd4f974282e722004c760e96ca65cceb95f69be
Travis Silvers <firestrand@gmail.com>
2011-08-11 07:18:57 -0400

    Refactoring Problem to strategy pattern

---
+++ VPSO/Problem/CEC2005F1Circle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VPSO.Problem
+{
+    public class CEC2005F1Circle : ProblemBase
+    {
+        public CEC2005F1Circle()
+        {
+            SwarmSize.D = 30;//30; 
+            for (int d = 0; d < SwarmSize.D; d++)
+            {
+                SwarmSize.min[d] = -100;
+                SwarmSize.max[d] = 100;
+                SwarmSize.q.Q[d] = 0;
+                SwarmSize.maxS[d] = SwarmSize.max[d];
+                SwarmSize.minS[d] = SwarmSize.min[d];
+                SwarmSize.maxInit[d] = SwarmSize.max[d];
+                SwarmSize.minInit[d] = SwarmSize.min[d];
+            }
+            EvaluationMaximum = SwarmSize.D * 10000;
+            EvaluationMaximum = 1000;
+            Epsilon = 0.000001;	//Acceptable error
+            ObjectiveValue = -450;       // Objective value
+
+            SwarmSize.q.Size = SwarmSize.D;
+        }
+        public override Fitness Evaluate(Position x)
+        {
+            var offset0 = new[]{ 
+                                    -3.9311900e+001, 5.8899900e+001, -4.6322400e+001, -7.4651500e+001, -1.6799700e+001,
+                                    -8.0544100e+001, -1.0593500e+001, 2.4969400e+001, 8.9838400e+001, 9.1119000e+000, 
+                                    -1.0744300e+001, -2.7855800e+001, -1.2580600e+001, 7.5930000e+000, 7.4812700e+001,
+                                    6.8495900e+001, -5.3429300e+001, 7.8854400e+001, -6.8595700e+001, 6.3743200e+001, 
+                                    3.1347000e+001, -3.7501600e+001, 3.3892900e+001, -8.8804500e+001, -7.8771900e+001, 
+                                    -6.6494400e+001, 4.4197200e+001, 1.8383600e+001, 2.6521200e+001, 8.4472300e+001
+                                };
+            double f = -450;
+            for (int d = 0; d < x.size; d++)
+            {
+                double xd = x.x[d];
+                x.x[d] = xd - offset0[d];
+                f = f + xd * xd;
+            }
+            var ff = new Fitness(Constants.fMax);
+            ff.f[0] = Math.Abs(f - ObjectiveValue);
+            return ff;
+        }
+    }
+}
--- VPSO/Problem/ProblemBase.cs
+++ VPSO/Problem/ProblemBase.cs
@@ -20,30 +20,21 @@ namespace VPSO.Problem
         {
             Solution = new Position(Constants.DMax);
             SwarmSize = new SwarmSize(Constants.DMax);
-            int d;
-            int initDiff = 0; // For each function, you can define a specific 
-            // initialisation space. In that case, you also have
-            // to set initDiff to a non zero value
-            int scanNb;
-            float z = 0.0f;
 
-            int nAtoms; // For Lennard-Jones problem
-            double[] lennard_jones = new[]
-                                         {-1, -3, -6, -9.103852, -12.71, -16.505384,-19.821489,-24.113360,-28.422532,
-                                          -32.77,-37.97,-44.33,-47.84,-52.32};
             Epsilon = 0.00000;		// Acceptable error. Defalut value
             ObjectiveValue = 0;       // Objective value. Default value
             Constraint = 0; 				// Number of constraints. Default value
             SwarmSize.valueNb = 0;
 
-
             // Define the solution point, for test
             // NEEDED when Parameters.stop = 2 
             // i.e. when stop criterion is distance_to_solution < epsilon
-            for (d = 0; d < 30; d++)
+            for (int d = 0; d < 30; d++)
             {
                 Solution.x[d] = 0;
             }
+
+            
         }
 
         public abstract Fitness Evaluate(Position x);
